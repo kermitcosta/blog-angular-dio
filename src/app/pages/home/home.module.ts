b469cde54a7d994993a3d29b9cbd65d0b2d738f7
Kermit Costa <kermitcosta@Kermits-MacBook-Air.local>
2023-04-05 21:34:29 -0300

    creation and initial style for hero component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HeroComponent } from '../../components/hero/hero.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeroComponent
   ],
   exports: [
     HomeComponent
