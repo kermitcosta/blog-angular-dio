f488c2285bb71e1e82578214a283b7ef0d9e75ee
Kermit Costa <kermitcosta@Kermits-MacBook-Air.local>
2023-04-06 21:05:31 -0300

    hero component styling and featured posts component created

---
+++ src/app/pages/home/featured-posts/featured-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-posts',
+  templateUrl: './featured-posts.component.html',
+  styleUrls: ['./featured-posts.component.scss']
+})
+export class FeaturedPostsComponent {
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { HeroComponent } from '../../components/hero/hero.component';
+import { HeroComponent } from '../home/hero/hero.component';
+import { FeaturedPostsComponent } from './featured-posts/featured-posts.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeroComponent
+    HeroComponent,
+    FeaturedPostsComponent
   ],
   exports: [
     HomeComponent
